16a316cc9603ea1b6a082ac97d99a97fc829b3d6
Shawn Larson <slarson62@gmail.com>
2019-11-27 14:40:25 -0700

    fix practice chart

--- src/app/shared/models/practice.ts
+++ src/app/shared/models/practice.ts
@@ -1,3 +1,5 @@
+import {basePieChartOptions} from './base-chart';
+
 export interface IPractice {
   firm_id: number;
   firm_name: string;
@@ -10,26 +12,14 @@ export interface IPractice {
   total_expenses: number;
   y: number;
 }
-export const practicePieChartOptions = {
-  chart: {
-    height: 300,
-    type: 'pie',
-    borderWidth: 0,
-    marginLeft: 120,
-    marginTop: 10,
-    spacingTop: 0,
-    spacingBottom: 15,
-    spacingLeft: 10,
-    spacingRight: 10,
-  },
-  credits: {
-    enabled: false
-  },
-  title: {text: null},
-  tooltip: {
+
+const practiceAdditionalOptions = {
+  tooltip : {
     useHTML: true,
     shared: true,
+    backgroundColor: 'white',
     headerFormat: null,
+    padding: 0,
     pointFormat: '<div style="width:250px;background-color:#f9b362;font-color:#fff;"><div><b>{series.name}</b><br /></div>' +
       '<div class="mb10">{point.practice_area}</div></div>' +
       '<div>Spend</div></div>' +
@@ -38,43 +28,12 @@ export const practicePieChartOptions = {
       '<div class="mb10">{point.firm_name}</div></div>' +
       '<div>Top Matter</div></div>' +
       '<div class="mb10">{point.matter_name}</div></div>'
-  },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-      colors: ['#9D02FE', '#6F00FF', '#4000FF', '#1102FF', '#0F1FFF', '#0C4CFF', '#037BFF', '#00AAFF', '#00C1FF', '#00D9FE'],
-      dataLabels: {
-        enabled: false
-      },
-      showInLegend: true
-    }
-  },
-  legend: {
-    show: true,
-    align: 'left',
-    verticalAlign: 'top',
-    layout: 'vertical',
-    x: 0,
-    y: 15,
-    useHTML: true,
-    symbolHeight: .001,
-    symbolWidth: .001,
-    symbolRadius: .001,
-    /* tslint:disable */
-    labelFormatter: function() {
-      const bgcolor = this.options.visible===false ? "#ccc" : this.color;
-      return '<div>' +
-        '<span class="hc-partner-badge" style="color:#FFF; background-color: ' + bgcolor + ';"> ' + this.practice_area +  ' </span>' +
-        '</div>';
     },
-    /* tslint:enable */
-    itemMarginBottom: 0
-  },
-  series: [{
-    name: 'Practice Area',
-    colorByPoint: true,
-    data: []
-  }]
-
+    series: [{
+      name: 'Practice Areas',
+      colorByPoint: true,
+      data: []
+    }]
 };
+export const practicePieChartOptions = { ... basePieChartOptions, ... practiceAdditionalOptions };
+
