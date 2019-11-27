f39997752a0d10cc748f44136f39c3182b118816
Shawn Larson <slarson62@gmail.com>
2019-11-27 14:38:10 -0700

    merge

--- src/app/shared/models/top-lead-partner.ts
+++ src/app/shared/models/top-lead-partner.ts
@@ -1,3 +1,5 @@
+import {basePieChartOptions} from './base-chart';
+
 export interface ITopLeadPartner {
   id: string;
   name: string;
@@ -20,69 +22,25 @@ export interface ITopLeadPartner {
   y: number;
 }
 
-export const leadPartnerChartOptions = {
-  chart: {
-    height: 300,
-    type: 'pie',
-    borderWidth: 0,
-    marginLeft: 120,
-    marginTop: 10,
-    spacingTop: 0,
-    spacingBottom: 15,
-    spacingLeft: 10,
-    spacingRight: 10,
-  },
-  credits: {
-    enabled: false
-  },
-  title: {text: null},
-  tooltip: {
+const leadPartnerAdditionalOptions = {
+  tooltip : {
     useHTML: true,
     shared: true,
+    backgroundColor: 'white',
     headerFormat: null,
+    padding: 0,
     pointFormat: '<div style="width:250px;background-color:#f9b362;font-color:#fff;"><div><b>{series.name}</b><br /></div>' +
       '<div class="mb10">{point.name}</div></div>' +
       '<div>Spend</div></div>' +
       '<div class="mb10">${point.total_billed:,.0f}</div></div>' +
       '<div>Practice Area</div></div>' +
       '<div class="mb10">{point.top_practice}</div></div>'
-  },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-      colors: ['#9D02FE', '#6F00FF', '#4000FF', '#1102FF', '#0F1FFF', '#0C4CFF', '#037BFF', '#00AAFF', '#00C1FF', '#00D9FE'],
-      dataLabels: {
-        enabled: false
-      },
-     showInLegend: true
-    }
-  },
-  legend: {
-    show: true,
-    align: 'left',
-    verticalAlign: 'top',
-    layout: 'vertical',
-    x: 0,
-    y: 15,
-    useHTML: true,
-    symbolHeight: .001,
-    symbolWidth: .001,
-    symbolRadius: .001,
-    /* tslint:disable */
-    labelFormatter: function() {
-      const bgcolor = this.options.visible===false ? "#ccc" : this.color;
-      return '<div>' +
-        '<span class="hc-partner-badge" style="color:#FFF; background-color: ' + bgcolor + ';"> ' + this.name +  ' </span>' +
-        '</div>';
     },
-    /* tslint:enable */
-    itemMarginBottom: 0
-  },
-  series: [{
-    name: 'Lead Partners',
-    colorByPoint: true,
-    data: []
-  }]
-
+    series: [{
+      name: 'Top Matters',
+      colorByPoint: true,
+      data: []
+    }]
 };
+export const leadPartnerChartOptions = { ... basePieChartOptions, ... leadPartnerAdditionalOptions };
+
