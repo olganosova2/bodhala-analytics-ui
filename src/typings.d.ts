b5c170325a935ea2d5c3630564bbe42a60352247
olga <olga.nosova@bodhala.com>
2019-11-27 14:35:02 -0500

    PLAT-195 fix unit test with 'require'

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,14 @@
 /* SystemJS module definition */
-declare var module: NodeModule;
+interface AppRequire {
+  <T>(path: string): T | any;
+  (paths: string[], callback: (...modules: any[]) => void): void | any;
+  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, chunkName?: string) => void | any;
+  context: any;
+}
+
+// tslint:disable-next-line:no-empty-interface
+interface NodeRequire extends AppRequire {}
+// declare var module: {
+//   id: string;
+// };
 declare var require: NodeRequire;
