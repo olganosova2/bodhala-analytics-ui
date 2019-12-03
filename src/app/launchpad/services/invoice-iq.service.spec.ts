3e2d5b783f57dbea8af6313b5b9422a0c4605563
Shawn Larson <slarson62@gmail.com>
2019-12-03 14:26:10 -0700

    fix pipeline

--- src/app/launchpad/services/invoice-iq.service.spec.ts
+++ src/app/launchpad/services/invoice-iq.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { DECLARATIONS, IMPORTS, PROVIDERS, SCHEMAS } from '../../shared/unit-tests/mock-app.imports';
 import { InvoiceIqService } from './invoice-iq.service';
 
 describe('InvoiceIqService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: IMPORTS,
+      declarations: DECLARATIONS,
+      providers: PROVIDERS,
+      schemas: SCHEMAS
+    });
+  });
 
   it('should be created', () => {
     const service: InvoiceIqService = TestBed.get(InvoiceIqService);
