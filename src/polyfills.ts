4afc3329be6486aa216874bf83db8d260ba6513c
olga <olga.nosova@bodhala.com>
2020-04-29 14:05:21 -0400

    PLAT-295 custom elements polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,8 +56,21 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'core-js/es/reflect';
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
+import '@webcomponents/custom-elements/custom-elements.min';
+import '@webcomponents/custom-elements/src/native-shim';
+
+
+
+
+if (!window['customElements']) {
+  const script = document.createElement('script');
+  script.src = './assets/webcomponentsjs/bundles/webcomponents-sd-ce.js';
+  document.writeln(script.outerHTML);
+}
