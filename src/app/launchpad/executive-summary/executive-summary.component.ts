104a91a91393416bc6464b89495b906d76a0f3fb
James Demery <james.demery@bodhala.com>
2020-07-21 12:49:27 -0400

    ES bugs and modifications.

--- src/app/launchpad/executive-summary/executive-summary.component.ts
+++ src/app/launchpad/executive-summary/executive-summary.component.ts
@@ -4,6 +4,10 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {AppStateService, HttpService, UserService} from 'bodhala-ui-common';
 import {FiltersService} from '../../shared/services/filters.service';
 import {Subscription} from 'rxjs';
+import { DatePipe } from '@angular/common';
+import * as _moment from 'moment';
+
+const moment = _moment;
 
 @Component({
   selector: 'bd-executive-summary',
@@ -14,6 +18,8 @@ export class ExecutiveSummaryComponent implements OnInit {
   pendingRequest: Subscription;
   errorMessage: any;
   maxDate: string;
+  lowerDateRange: string;
+  upperDateRange: string;
   // errorMessage: any;
   // summary: any;
   // isLoaded: boolean = false;
@@ -34,15 +40,25 @@ export class ExecutiveSummaryComponent implements OnInit {
               public appStateService: AppStateService,
               public filtersService: FiltersService,
               public userService: UserService,
-              public commonServ: CommonService) {
+              public commonServ: CommonService,
+              public datepipe: DatePipe) {
   }
 
   ngOnInit() {
     const params = this.filtersService.getCurrentUserCombinedFilters();
+    params.startdate = '';
+    params.enddate = '';
     this.pendingRequest = this.httpService.makeGetRequest('getDateRange', params).subscribe(
       (data: any) => {
         if (data) {
           this.maxDate = data.result.max;
+
+          const lastYear = moment(this.maxDate).year();
+          const d = new Date(lastYear, 0 , 1);
+          const janOne = new Date(d).toISOString().slice(0, 10);
+          this.lowerDateRange = this.datepipe.transform(janOne, 'mediumDate');
+          this.upperDateRange = this.datepipe.transform(this.maxDate, 'mediumDate');
+
         }
       },
       err => {
