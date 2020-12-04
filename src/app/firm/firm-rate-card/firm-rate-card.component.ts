aa9c077bb894051c5247f4898308088eeb74b427
James Demery <james.demery@bodhala.com>
2020-12-04 17:28:38 -0500

    PLAT632

--- src/app/firm/firm-rate-card/firm-rate-card.component.ts
+++ src/app/firm/firm-rate-card/firm-rate-card.component.ts
@@ -47,6 +47,8 @@ export class FirmRateCardComponent implements OnInit, OnDestroy {
   comparing: boolean = false;
   newStartDate: string;
   percentOfTotal: number;
+  minMatterCost: number;
+  maxMatterCost: number;
   rank: number;
   selectedSavedFilterName: string = null;
   logoUrl: string;
@@ -309,6 +311,14 @@ export class FirmRateCardComponent implements OnInit, OnDestroy {
   changeTab(evt): void {
     this.selectedTabIndex = evt.index;
     if (this.selectedTabIndex === 1) {
+      const tempFilters = localStorage.getItem('ELEMENTS_dataFilters_' + this.userService.currentUser.id.toString());
+      const tempFiltersDict = JSON.parse(tempFilters);
+      for (const filter of tempFiltersDict.dataFilters) {
+        if (filter.fieldName === 'matterCost') {
+          this.minMatterCost = filter.value[0];
+          this.maxMatterCost = filter.value[1];
+        }
+      }
       const dates = this.filtersService.parseLSDateString();
       const params = {clientId: this.userService.currentUser.client_info.id};
       this.pendingRequest = this.httpService.makeGetRequest('getDateRange', params).subscribe(
@@ -330,6 +340,13 @@ export class FirmRateCardComponent implements OnInit, OnDestroy {
         if (filter.fieldName === 'dateRange') {
           filter.value.startDate = this.reportCardStartDate;
           filter.value.endDate = this.reportCardEndDate;
+        } else if (filter.fieldName === 'matterCost') {
+          console.log("filter: ", filter);
+          if (filter.value[0] !== this.minMatterCost) {
+            filter.value[0] = this.minMatterCost;
+          } else if (filter.value[1] !== this.maxMatterCost) {
+            filter.value[1] = this.maxMatterCost;
+          }
         }
       }
       tempFiltersDict.datestring = '&startdate=' + this.reportCardStartDate + '&enddate=' + this.reportCardEndDate;
