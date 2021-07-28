9936ef3e2cbb44817f4078c56010f0fe9eb21a7e
olga <olga.nosova@bodhala.com>
2021-07-28 17:34:13 -0400

    PLAT-928 - fix conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -242,7 +242,7 @@ export function initHttp(service: HttpService) {
     CheckboxCellComponent,
     LedesImportsComponent,
     ImportDetailComponent,
-    RerunUploadComponent
+    RerunUploadComponent,
     CheckboxCellComponent,
     YoyRateIncreaseComponent
   ],
