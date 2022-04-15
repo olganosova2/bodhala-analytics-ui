7bea4c2a9b2eaf844590f1b164d0af472f1743d2
olga <olga.nosova@bodhala.com>
2022-04-15 14:57:50 -0400

    PLAT-1197 unit tests

--- src/app/admin/insights/matter-insights/matter-insights.component.spec.ts
+++ src/app/admin/insights/matter-insights/matter-insights.component.spec.ts
@@ -7,6 +7,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import * as mockServices from '../../../shared/unit-tests/mock-services';
 import {FiltersService} from '../../../shared/services/filters.service';
 import {CommonService} from '../../../shared/services/common.service';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatSelectChange} from '@angular/material/select';
 
 describe('MatterInsightsComponent', () => {
   let component: MatterInsightsComponent;
@@ -58,7 +60,38 @@ describe('MatterInsightsComponent', () => {
   });
   it('should getMatterInsight', () => {
     component.firm = { id: '47', name: 'Kirkland'};
+    component.selectedClientId = 110;
+    component.matterId = '777';
     component.getMatterInsight();
-    expect(result).toBe(47);
+    expect(component.matterSelected).toBeTruthy();
+  });
+  it('should loadMatters when valid input', () => {
+    component.selectedClientId = 110;
+    component.loadMatters('777');
+    expect(component.filteredNames.length).toBe(2);
+  });
+  it('should loadMatters when invalid input', () => {
+    component.selectedClientId = 110;
+    component.loadMatters('77');
+    expect(component.filteredNames.length).toBe(0);
+  });
+  it('should loadFirms', () => {
+    spyOn(component, 'getMatterInsight').and.callThrough();
+    component.selectedClientId = 110;
+    const params = { option: { value: { id: 100 }} } as MatAutocompleteSelectedEvent;
+    component.loadFirms(params);
+    expect(component.getMatterInsight).toHaveBeenCalled();
+  });
+  it('should getMatterId', () => {
+    component.matterId = '777';
+    const result = component.getMatterId();
+    expect(result).toBe('777');
+  });
+  it('should getDataByMatterAndFirm', () => {
+    spyOn(component, 'getMatterInsight').and.callThrough();
+    const params = {  value: { id: 100 }}  as MatSelectChange;
+    component.matterId = '777';
+    component.getDataByMatterAndFirm(params);
+    expect(component.getMatterInsight).toHaveBeenCalled();
   });
 });
