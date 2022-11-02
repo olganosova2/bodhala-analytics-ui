6d793bb3fbd36a318f4c0c87c3431038d74c4813
olga <olga.nosova@bodhala.com>
2022-11-02 16:24:43 -0400

    PLAT-1423, refactor mockRouter start

--- src/app/qbr/qbr-top-pas/qbr-top-pas-firms/qbr-top-pas-firms.component.spec.ts
+++ src/app/qbr/qbr-top-pas/qbr-top-pas-firms/qbr-top-pas-firms.component.spec.ts
@@ -1,11 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { QbrTopPasFirmsComponent } from './qbr-top-pas-firms.component';
 import {DECLARATIONS, IMPORTS, PROVIDERS, SCHEMAS} from '../../../shared/unit-tests/mock-app.imports';
-import {QbrTimekeepersComponent} from '../qbr-timekeepers/qbr-timekeepers.component';
 import {AppStateService, HttpService, UserService} from 'bodhala-ui-common';
 import {ActivatedRoute, Router} from '@angular/router';
-import {ActivatedRouteMock} from '../../../shared/unit-tests/mock-services';
 import * as mockServices from '../../../shared/unit-tests/mock-services';
 import {FiltersService} from '../../../shared/services/filters.service';
 import {MOCK_QBR_DATA, MOCK_QBRS} from '../../../shared/unit-tests/mock-data/qbr-executive-summary';
@@ -28,7 +25,7 @@ describe('QbrTopPasFirmsComponent', () => {
         providers: [
           AppStateService,
           { provide: Router, useClass: mockServices.MockRouter},
-          { provide: ActivatedRoute, useClass: ActivatedRouteMock },
+          { provide: ActivatedRoute, useClass: mockServices.ActivatedRouteMock },
           { provide: FiltersService, useClass: mockServices.FiltersStub },
           { provide: HttpService, useClass: mockServices.DataStub },
           { provide: UserService, useClass: mockServices.UserStub }
--- src/app/qbr/qbr-top-pas/qbr-top-pas-matters/qbr-top-pas-matters.component.spec.ts
+++ src/app/qbr/qbr-top-pas/qbr-top-pas-matters/qbr-top-pas-matters.component.spec.ts
@@ -2,10 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QbrTopPasMattersComponent } from './qbr-top-pas-matters.component';
 import {DECLARATIONS, IMPORTS, PROVIDERS, SCHEMAS} from '../../../shared/unit-tests/mock-app.imports';
-import {QbrTopPasFirmsComponent} from '../qbr-top-pas-firms/qbr-top-pas-firms.component';
 import {AppStateService, HttpService, UserService} from 'bodhala-ui-common';
 import {ActivatedRoute, Router} from '@angular/router';
-import {ActivatedRouteMock} from '../../../shared/unit-tests/mock-services';
 import * as mockServices from '../../../shared/unit-tests/mock-services';
 import {FiltersService} from '../../../shared/services/filters.service';
 import {MOCK_QBR_DATA, MOCK_QBRS} from '../../../shared/unit-tests/mock-data/qbr-executive-summary';
@@ -28,7 +26,7 @@ describe('QbrTopPasMattersComponent', () => {
         providers: [
           AppStateService,
           { provide: Router, useClass: mockServices.MockRouter},
-          { provide: ActivatedRoute, useClass: ActivatedRouteMock },
+          { provide: ActivatedRoute, useClass: mockServices.ActivatedRouteMock },
           { provide: FiltersService, useClass: mockServices.FiltersStub },
           { provide: HttpService, useClass: mockServices.DataStub },
           { provide: UserService, useClass: mockServices.UserStub }
