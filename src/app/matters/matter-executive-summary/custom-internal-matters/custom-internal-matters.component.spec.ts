4265856604ca1c9ecd2a5cafb3842e006491f38d
olga <olga.nosova@bodhala.com>
2022-11-03 11:03:34 -0400

    PLAT-1423, fix lint

--- src/app/matters/matter-executive-summary/custom-internal-matters/custom-internal-matters.component.spec.ts
+++ src/app/matters/matter-executive-summary/custom-internal-matters/custom-internal-matters.component.spec.ts
@@ -32,7 +32,7 @@ describe('CustomInternalMattersComponent', () => {
       set: {
         providers: [
           AppStateService,
-          { provide: ActivatedRoute, useClass:mockServices.ActivatedRouteMock},
+          { provide: ActivatedRoute, useClass: mockServices.ActivatedRouteMock},
           { provide: FiltersService, useClass: mockServices.FiltersStub },
           { provide: HttpService, useClass: mockServices.DataStub },
           { provide: UserService, useClass: mockServices.UserStub },
